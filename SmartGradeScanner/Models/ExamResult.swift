a0035410c5ff6827dfe26691a9207c5ef0de14c7
asdasw22 <melyacindy19@gmail.com>
2026-08-29 00:15:47 +0300

    Update ExamResult.swift

--- SmartGradeScanner/Models/ExamResult.swift
+++ SmartGradeScanner/Models/ExamResult.swift
@@ -19,10 +19,13 @@ import SwiftData
     @Relationship(deleteRule: .cascade) var responses: [StudentResponse]
 
     init(omrResult: OMRProcessingResult, exam: Exam? = nil, student: Student? = nil, maximumScore: Double? = nil) {
+        let finalMaximumScore = maximumScore ?? Double(max(omrResult.questions.count, 1))
+        let finalScore = omrResult.earnedScore
+
         self.id = UUID(); self.scannedAt = .now; self.studentID = omrResult.studentID ?? "Unknown"
-        self.student = student; self.exam = exam; self.score = omrResult.earnedScore
-        self.maximumScore = maximumScore ?? Double(max(omrResult.questions.count, 1))
-        self.percentage = self.maximumScore > 0 ? (self.score / self.maximumScore) * 100 : 0
+        self.student = student; self.exam = exam; self.score = finalScore
+        self.maximumScore = finalMaximumScore
+        self.percentage = finalMaximumScore > 0 ? (finalScore / finalMaximumScore) * 100 : 0
         self.correctCount = omrResult.correctCount; self.wrongCount = omrResult.wrongCount
         self.emptyCount = omrResult.emptyCount; self.multipleCount = omrResult.multipleCount
         self.needsReview = omrResult.needsReview; self.correctedImageData = omrResult.alignedImageData
