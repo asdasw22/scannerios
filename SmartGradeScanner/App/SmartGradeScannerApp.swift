6f44f66efc71f9788f89539c75220c24d10f21ec
asdasw22 <melyacindy19@gmail.com>
2026-08-28 22:47:09 +0300

    Update SmartGradeScannerApp.swift

--- SmartGradeScanner/App/SmartGradeScannerApp.swift
+++ SmartGradeScanner/App/SmartGradeScannerApp.swift
@@ -3,14 +3,26 @@ import SwiftData
 
 @main
 struct SmartGradeScannerApp: App {
+
     let container: ModelContainer
+
     init() {
-        do { container = try ModelContainerFactory.make() }
-        catch { fatalError("Unable to create SmartGrade storage: \(error)") }
+        do {
+            container = try ModelContainerFactory.make()
+        } catch {
+            fatalError("Unable to create SmartGrade storage: \(error)")
+        }
     }
+
     var body: some Scene {
-        WindowGroup { RootView() }
-            .modelContainer(container)
-            .task { SampleDataSeeder.seedIfNeeded(in: container.mainContext) }
+        WindowGroup {
+            RootView()
+                .task {
+                    SampleDataSeeder.seedIfNeeded(
+                        in: container.mainContext
+                    )
+                }
+        }
+        .modelContainer(container)
     }
 }
